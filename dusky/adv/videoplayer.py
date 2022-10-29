737ad9bd400b41748a6f799d01b4caf923cc0b17
Dusky Joe (Eshwaran) <116634369+DuskyMusic@users.noreply.github.com>
2022-10-29 11:51:24 -0700

    Create videoplayer.py

---
+++ dusky/adv/videoplayer.py
@@ -0,0 +1,23 @@
+__MODULE__ = "Video Play"
+__HELP__ = """
+
+Dusky can play video in your group's voice chat 
+zero lag time with high quality
+
+× /vplay - `[Youtube URL/Search Query/live URL or reply video/audion]` 
+× /vpause - Pause the playing music on voice chat.
+× /vresume - Resume the paused music on voice chat.
+× /vskip- Skip the current playing music on voice chat
+× /vend or /stop- Stop the playout.
+× /queue- Check queue list.
+× /playplaylist - Start playing Your Saved Playlist.
+× /playlist - Check Your Saved Playlist On Servers.
+× /delmyplaylist- Delete any saved music in your playlist
+× /delgroupplaylist- Delete any saved music in your group's playlist [Requires Admin Rights.]
+
+This is most advance music player in telegram base on private Socure 
+music player repo Dusky can play music or video in your group video chat without lag...!
+
+
+ """
+__advtools__ = __HELP__
